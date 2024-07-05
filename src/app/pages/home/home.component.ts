663133373ddd484b98debe64b6c7a9682c68f687
Seb4sC <sacastro081@soy.sena.edu.co>
2024-07-05 14:01:51 -0500

    update state con signals y funciones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from './../../models/task.model'
 
 @Component({
   selector: 'app-home',
@@ -9,24 +10,49 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
+  tasks = signal<Task[]>([
     {
       id: Date.now(),
-      title: 'Instalar',
+      title: 'Crear Proyecto',
       completed: false
     },
-    'Instalar el Angular CLI',
-    'Crear el proyecto',
-    'Crear componentes'
+    {
+      id: Date.now(),
+      title: 'Crear Componentes',
+      completed: false
+    }
   ]);
 
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string){
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   deleteTask(index: number){
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
   }
 }
